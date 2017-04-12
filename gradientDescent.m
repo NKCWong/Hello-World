d26be3a7314e687de7eadff62eebe7a789d1f5d3
NKCWong <NKCWong@users.noreply.github.com>
2017-04-11 20:11:47 -0700

    conflict

--- gradientDescent.m
+++ gradientDescent.m
@@ -12,8 +12,6 @@ for iter = 1:num_iters
 
     % ====================== YOUR CODE HERE ======================
     %
-    % Hint: While debugging, it can be useful to print out the values
-    %       of the cost function (computeCost) and gradient here.
     %
     hypothesis = X * theta;
     delta = zeros(length(theta), 1);
